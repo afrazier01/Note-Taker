3d514a32ef5cb58b2807ccedc6ec58e3cb8f4696
afrazier01 <amfrazier01@gmail.com>
2023-05-26 23:38:22 -0500

    added api notes route with json response

--- routes/note.js
+++ routes/note.js
@@ -1,1 +1,17 @@
-const 
+const router = require('express').Router();
+
+router.get('/', (req,res) => {
+    console.info(`${req.method} request received`)
+
+    //test insomnia
+    res.json(`${req.method} request received`)
+});
+
+router.post('/', (req,res) => {
+    console.info(`${req.method} request received`)
+
+    //test insomnia
+    res.json(`${req.method} request received`)
+});
+
+module.exports = router;
