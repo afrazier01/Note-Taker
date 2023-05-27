d9318089493c5f04f23b015dd175285de03c2518
afrazier01 <amfrazier01@gmail.com>
2023-05-26 23:09:46 -0500

    added routes to send index and note html files

--- server.js
+++ server.js
@@ -1,1 +1,45 @@
 console.log("You're connected!")
+const routes = require('./routes/index')
+const path = require('path');
+
+const express = require('express');
+
+const PORT = process.env.port || 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+// app.use('/api', routes);
+
+
+
+//CRUD
+app.get('/', (req,res) => {
+    console.info(`${req.method} request received`)
+
+    res.sendFile(path.join(__dirname,'/public/index.html'))
+});
+
+app.post('/', (req,res) => {
+    console.info(`${req.method} request received`)
+
+    //test insomnia
+    res.json(`${req.method} request received`)
+});
+
+app.get('/notes', (req,res) => {
+    console.info(`${req.method} request received`)
+
+    res.sendFile(path.join(__dirname,'/public/notes.html'))
+});
+
+app.post('/notes', (req,res) => {
+    console.info(`${req.method} request received`)
+
+    //test insomnia
+    res.json(`${req.method} request received`)
+});
+
+app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT} 🚀`))
