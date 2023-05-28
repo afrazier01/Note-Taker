ab9135e3220d21474e0c0cca97faa7a339f22c9c
afrazier01 <amfrazier01@gmail.com>
2023-05-28 09:09:19 -0500

    Progress with saving notes to db.json

--- routes/note.js
+++ routes/note.js
@@ -1,27 +1,53 @@
 const router = require('express').Router();
-const { writeFile } = require('fs');
+const { writeFile, read } = require('fs');
 const {readFile} = require('fs/promises')
 const util = require('util')
+generateID =require('unique-id-generator')
 
 router.get('/', (req,res) => {
     console.info(`${req.method} request received`)
     readFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
+const listData = [];
 router.post('/', (req,res) => {
     console.info(`${req.method} request received`)
 
     let response;
-    const data = req.body
+    let newList;
+    
     if (req.body) {
         response = {
             status: 'success',
             data: req.body,
         };
 
-        writeFile('./db/db.json', JSON.stringify(data), (err) => {
-            err ? console.error(err) : console.info('Data successfully written')
+        newNote = req.body,generateID()
+    
+        console.info(newNote);
+        
+
+        readFile('./db/db.json').then((data) => {
+
+            const listData  = JSON.parse(data);
+            console.info(`\nThere are/is ${listData.length} notes currently`)
+            listData.push(newNote)
+            console.info(`You have added a new note! Now there are ${listData.length} notes`)
+            
+            //stringify new list
+            const newList = JSON.stringify(listData);
+            console.info(`:>>> See new stringify notes: ${newList}`)
+
+            //overwrite db.json file
+            writeFile('./db/db.json', newList , (err) => err ? console.error(err) : console.info('Data successfully written!'));
         });
+
+        
+
+        
+        // writeFile('./db/db.json', JSON.stringify(data), (err) => {
+        //     err ? console.error(err) : console.info('Data successfully written')
+        // });
 
         res.json(`A note containing the message '${response.data}' was received.`)
     } else {
